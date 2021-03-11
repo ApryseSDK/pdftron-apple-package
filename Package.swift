dcb6c6ac5358704efed21d57a8de8eb230f2599d
JamieDass <20866295+JamieDass@users.noreply.github.com>
2021-03-10 16:38:38 -0800

    Remove newline

--- Package.swift
+++ Package.swift
@@ -13,12 +13,10 @@ let package = Package(
         .binaryTarget(
             name: "PDFTron",
             url: "https://www.pdftron.com/downloads/ios/package/8.1.2/PDFTron.xcframework.zip",
-            checksum: "55b3f1f21ee5d9c9729bef8f69fde1d97bd14f407edb55b5f3e79c17ddd771b1
-"),
+            checksum: "55b3f1f21ee5d9c9729bef8f69fde1d97bd14f407edb55b5f3e79c17ddd771b1"),
         .binaryTarget(
             name: "PDFTronTools",
             url: "https://www.pdftron.com/downloads/ios/package/8.1.2/PDFTronTools.xcframework.zip",
-            checksum: "f8b90ea9ac8888a512863154c4d6173e5c9001cf79631bf878b750380391cddb
-"),
+            checksum: "f8b90ea9ac8888a512863154c4d6173e5c9001cf79631bf878b750380391cddb"),
     ]
 )
