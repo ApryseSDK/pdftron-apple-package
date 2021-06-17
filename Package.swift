7efe87c3de770f5fed3761be0959a69a17125fd2
Jamie Dassoulas <20866295+JamieDass@users.noreply.github.com>
2021-06-17 09:25:23 -0700

    Fix incorrect URL

--- Package.swift
+++ Package.swift
@@ -12,11 +12,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PDFNet",
-            url: "https://www.pdftron.com/downloads/ios/packages/9.1.0/PDFNet.xcframework.zip",
+            url: "https://www.pdftron.com/downloads/ios/packages/9.0.1/PDFNet.xcframework.zip",
             checksum: "12a8fbe63fd7a790090df01180254980b8b2c5085f6e51f55d405c5d86477435"),
         .binaryTarget(
             name: "Tools",
-            url: "https://www.pdftron.com/downloads/ios/packages/9.1.0/Tools.xcframework.zip",
+            url: "https://www.pdftron.com/downloads/ios/packages/9.0.1/Tools.xcframework.zip",
             checksum: "dba14970fc54b2342e8460e9946bc5fb76c129a6e8cdf439256213e2bdcbdaea"),
     ]
 )
