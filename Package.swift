b8e5a89c9e8d70dc0df4c6aeb4619841600a54fd
Darren Chan <chanthedarren@gmail.com>
2026-07-06 09:57:46 -0700

    Update for PDFTron iOS 12.0.030566

--- Package.swift
+++ Package.swift
@@ -12,11 +12,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PDFNet",
-            url: "https://www.pdftron.com/downloads/ios/packages/11.13.0-30701/PDFNet.xcframework.zip",
-            checksum: "6522827d133a2be9726122180e4192d73a446a5262062f64c64c4cda4a347ca4"),
+            url: "https://www.pdftron.com/downloads/ios/packages/12.0.0-30566/PDFNet.xcframework.zip",
+            checksum: "768391530d6609879348706c684b68a704a013b57d99f3dd154fa2f9373387d6"),
         .binaryTarget(
             name: "Tools",
-            url: "https://www.pdftron.com/downloads/ios/packages/11.13.0-30701/Tools.xcframework.zip",
-            checksum: "6bf6ab60d38d57bb9b1db79e2cfea059692811bb74d07e75cab0a03d8ce5ab61"),
+            url: "https://www.pdftron.com/downloads/ios/packages/12.0.0-30566/Tools.xcframework.zip",
+            checksum: "433a0b65c61c0542285e79d1c6de2a38c26426bf28467c05cb9313b69b8746d5"),
     ]
 )
